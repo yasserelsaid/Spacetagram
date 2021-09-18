22833959e990e521069daa9c608e07ba218382f2
Yasser Elsaeed <yassermelsaeed@gmail.com>
2021-09-18 13:30:05 -0400

    added refresh functionality

--- components/SpaceImages.js
+++ components/SpaceImages.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
-
+import { IconButton } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import { CircularProgress } from '@material-ui/core';
 import axios from 'axios';
 import SpaceImage from './SpaceImage';
 import { makeStyles } from '@material-ui/core/styles';
+import RefreshIcon from '@material-ui/icons/Cached';
 
 const useStyles = makeStyles(theme => ({
   progressContainer: {
     display: 'flex',
     justifyContent: 'center',
-    marginTop: '150px',
-    marginBottom: '150px',
+    margin: '200px 0',
+  },
+  refreshButtonContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingBottom: '20px',
   },
 }));
 function SpaceImages() {
@@ -24,9 +29,10 @@ function SpaceImages() {
       const url =
         'https://api.nasa.gov/planetary/apod?api_key=' +
         process.env.NEXT_PUBLIC_NASA_API_KEY +
-        '&count=4&thumbs=true';
+        '&count=6&thumbs=true';
       const { data } = await axios.get(url);
       setImages(data);
+      console.log(data);
     } catch (err) {
       console.error(err);
     } finally {
@@ -39,16 +45,25 @@ function SpaceImages() {
   }, []);
   return (
     <>
+      <div className={classes.refreshButtonContainer}>
+        <IconButton
+          variant='contained'
+          onClick={() => fetchImages()}
+          color='secondary'
+        >
+          <RefreshIcon />
+        </IconButton>
+      </div>
       {loading ? (
         <div className={classes.progressContainer}>
           <CircularProgress size={120} color='secondary' />
         </div>
       ) : (
-        <Grid container spacing={2}>
+        <Grid container spacing={3}>
           {images.map(
             image =>
               image.media_type === 'image' && (
-                <Grid item key={image.url} md={3}>
+                <Grid item key={image.url} md={4}>
                   <SpaceImage
                     title={image.title}
                     url={image.url}
